824e32b8f704e43221eea96f7a0e7786d35a0038
Un-dev <francois.lannoy@kelindi.com>
2022-03-01 10:25:26 +0100

    :white_check_mark: added budget test and fixtures
    test for #next and #previous

--- test/models/budget_test.rb
+++ test/models/budget_test.rb
@@ -1,16 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 class BudgetTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
 
-  test "#next" do
-    
-
+  setup do
+    @first_budget = Budget.find_by(starts_at: '01-01-2022')
+    @second_budget = Budget.find_by(starts_at: '01-02-2022')
+    @third_budget = Budget.find_by(starts_at: '01-03-2022')
+  end
+  test '#next' do
+    next_budget = Budget.next(@first_budget)
+    assert_equal @second_budget, next_budget
+    next_budget = Budget.next(@second_budget)
+    assert_equal @third_budget, next_budget
+    next_budget = Budget.next(@third_budget)
+    assert_equal @third_budget, next_budget
   end
 
-  test "#previous" do
-
+  test '#previous' do
+    previous_budget = Budget.previous(@first_budget)
+    assert_equal @first_budget, previous_budget
+    previous_budget = Budget.previous(@second_budget)
+    assert_equal @first_budget, previous_budget
+    previous_budget = Budget.previous(@third_budget)
+    assert_equal @second_budget, previous_budget
   end
 end
