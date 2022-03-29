1af201f636c20bff547e0818aa526b0278542ada
Un-dev <francois.lannoy@kelindi.com>
2022-03-29 09:28:05 +0200

    :white_check_mark: added fixtures to make tests go green

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,7 @@ class UserTest < ActiveSupport::TestCase
   setup { @user = User.find_by(surname: 'toto') }
 
   test '#total_assets' do
-    assert_equal 8705, @user.total_assets
+    assert_equal 2, @user.accounts.count
+    assert_equal 6000, @user.total_assets
   end
 end
