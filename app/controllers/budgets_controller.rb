0875b836661c72f22c721769a032d7c0efa53130
Un-dev <francois.lannoy@kelindi.com>
2022-03-29 11:14:47 +0200

    :fire: cleansed controller

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,15 +1,8 @@
 class BudgetsController < ApplicationController
-  before_action :find_budget, except: %i[index new create]
-  def index
-    if params[:budget_id].nil?
-      @current_budget = Budget.current_month_budget
-    else
-      @current_budget = Budget.find(params[:budget_id])
-    end
-    @expenses = @current_budget.expenses.includes(:category).all
-  end
+  # before_action :find_budget, except: %i[index new create]
+  def index; end
 
-  private def find_budget
-    @budget = Budget.find(params[:budget_id])
-  end
+  # private def find_budget
+  #   @budget = Budget.find(params[:budget_id])
+  # end
 end
