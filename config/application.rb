bcc30a3a03363a0bcec2e5a268f5052da2dc616f
Un-dev <francois.lannoy@kelindi.com>
2022-05-31 09:45:02 +0200

    :wrench: configured i18n

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +11,8 @@ module BudgetManagement
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :fr
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
