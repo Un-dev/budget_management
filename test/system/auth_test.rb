975babe47828a52b62059e6b0f5b0dc429e0d5f8
Un-dev <francois.lannoy@kelindi.com>
2022-06-01 10:34:51 +0200

    :white_check_mark: fixed faulty test

--- test/system/auth_test.rb
+++ test/system/auth_test.rb
@@ -14,13 +14,12 @@ class AuthTest < ApplicationSystemTestCase
   test 'registration should redirect to Account creation' do
     visit '/'
     click_on 'Sign up'
-    fill_in 'user_email', with: 'new@user.com'
+    fill_in 'user_email', with: 'new@user.fr'
     fill_in 'user_name', with: 'new'
     fill_in 'user_surname', with: 'user'
     fill_in 'user_password', with: 'newuser'
     fill_in 'user_password_confirmation', with: 'newuser'
-    click_on 'Sign up'
-
+    find('#confirm-signup').click
     assert_text 'Nouveau compte'
   end
 end
